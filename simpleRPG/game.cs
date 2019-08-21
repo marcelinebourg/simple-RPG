a5b718e6b3dd543c82ead3828260983f5fb82ceb
Gabriel Bourg <s198007@students.aie.edu>
2019-08-21 09:14:58 -0500

    Add project files.

---
+++ simpleRPG/game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleRPG
+{
+    class Game
+    {
+        public void Start()
+        {
+            Console.WriteLine("Welcome!");
+            Console.ReadKey();
+        }
+    }
+}
